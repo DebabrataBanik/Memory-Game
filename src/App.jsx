18c42ef2c1b6defabb6f5853699289d70d1e1453
DebabrataBanik <debabratabanik02@gmail.com>
2025-01-25 16:25:04 +0530

    emoji array items duplicate and shuffle logic

--- src/App.jsx
+++ src/App.jsx
@@ -15,13 +15,45 @@ function App() {
         throw new Error("Could not fetch data")
       } else {
         const data = await response.json()
-        setEmojiArray(data.slice(0, 5))
+        const emojisArray = getEmojisArray(data)
+        setEmojiArray(emojisArray)
         setIsGameOn(true)
       }
     }
     catch (error) {
       console.error(error)
     }
+  }
+
+  function getEmojisArray(data) {
+    const randomIndices = getRandomIndices(data);
+    const emojiArr = randomIndices.map(index => data[index]);
+    const pairedEmojisArray = [...emojiArr, ...emojiArr]
+
+    // Fisher-Yates algorithm to shuffle array
+    for (let i = pairedEmojisArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1))
+      const temp = pairedEmojisArray[i]
+      pairedEmojisArray[i] = pairedEmojisArray[j]
+      pairedEmojisArray[j] = temp
+    }
+    return pairedEmojisArray
+  }
+
+  // generating random indices to get random emojis
+  function getRandomIndices(data, count = 5) {
+    // return Array.from({ length: 5 }, () => Math.floor(Math.random() * data.length))
+
+    const randomIndices = []
+    for (let i = 0; i < count; i++) {
+      const randomIndex = Math.floor(Math.random() * data.length)
+      if (!randomIndices.includes(randomIndex)) {
+        randomIndices.push(randomIndex)
+      } else {
+        i--
+      }
+    }
+    return randomIndices
   }
 
   return (
